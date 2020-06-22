31204c6ad2f3b43af4a63982b1e8bba1cae8a734
willie <ss3751@gmail.com>
2020-06-22 23:28:07 +0800

    stopPropagation - 中止冒泡事件

--- all.js
+++ all.js
@@ -1,11 +1,11 @@
 var el=document.querySelector('.box');
-el.addEventListener('click',function(){
+el.addEventListener('click',function(e){e.stopPropagation();
     alert('box');
     console.log('box')
-},true);
+},false);
 
 var elbody=document.querySelector('.body');
 elbody.addEventListener('click',function(){
     alert('body');
     console.log('body');
-},true);
+},false);
