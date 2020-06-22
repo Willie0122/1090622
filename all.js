222418dae0f627606ee8164b6d75513255b1ea81
willie <ss3751@gmail.com>
2020-06-22 23:59:36 +0800

    preventDefault - 取消預設觸發行為

--- all.js
+++ all.js
@@ -1,11 +1,8 @@
-var el=document.querySelector('.box');
-el.addEventListener('click',function(e){e.stopPropagation();
-    alert('box');
-    console.log('box')
-},false);
-
 var elbody=document.querySelector('.body');
-elbody.addEventListener('click',function(){
+elbody.addEventListener('click',function(e){
+    e.preventDefault();
+    //取消元素的默認行為
+    //原本點連結會跳轉到指定網頁
     alert('body');
     console.log('body');
 },false);
