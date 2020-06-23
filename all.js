a84b68db5201d1f2214251245929dbf2a600d130
willie <ss3751@gmail.com>
2020-06-23 08:05:59 +0800

    e.target - 了解目前所在元素位置

--- all.js
+++ all.js
@@ -1,8 +1,4 @@
 var elbody=document.querySelector('.body');
 elbody.addEventListener('click',function(e){
-    e.preventDefault();
-    //取消元素的默認行為
-    //原本點連結會跳轉到指定網頁
-    alert('body');
-    console.log('body');
-},false);
+    console.log(e.screenX);   
+},false)
