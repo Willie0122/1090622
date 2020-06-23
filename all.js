556df2bcb5bbfb6de1f3b32e87ae73b7b01e6a55
willie <ss3751@gmail.com>
2020-06-23 09:03:21 +0800

    change-表單內容更動內容時觸發

--- all.js
+++ all.js
@@ -1,4 +1,29 @@
-var elbody=document.querySelector('.body');
-elbody.addEventListener('click',function(e){
-    console.log(e.screenX);   
-},false)
+var el = document.querySelector('#select');
+var ul = document.querySelector('.list');
+
+var Group = [{
+    Name: 'AOAO',
+    Age: '中壢區'
+},
+{
+    Name: 'BOBO',
+    Age: '20'
+},
+{
+    Name: 'COCO',
+    Age: '中壢區'
+}];
+
+var len=Group.length;
+var str='';
+
+function updatelist(e){
+    var select=e.target.value;
+    for(i=0;i<len;i++){
+        if(select==Group[i].Age){
+            str+='<li>'+Group[i].Name+'</li>';
+        }
+    }
+    ul.innerHTML=str;
+}
+el.addEventListener('change',updatelist,false);
