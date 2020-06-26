ff1ffbfb846429e395d15d276457f4f77ad014e4
willie <ss3751@gmail.com>
2020-06-26 21:23:09 +0800

    blur - 離開焦點時進行事件觸發

--- all.js
+++ all.js
@@ -1,19 +1,28 @@
-var table=document.querySelector('.table');
-var content=document.querySelector('.content');
+var ham=document.getElementById('hamID');
+var fri=document.getElementById('friID');
+ham.addEventListener('blur',hamfun,false);
+fri.addEventListener('blur',frifun,false);
 
-table.addEventListener('change',CL,false);
-
-function CL(e){
-    mo=e.target.value;
-    if(mo=="中壢區"){
-        content.innerHTML="中壢區為臺灣桃園市13區之一。中壢區位於全市平地的中心位置，由北開始順時針依序與大園區、蘆竹區、桃園區、八德區、平鎮區、楊梅區、新屋區、觀音區共8區接壤，鄰接行政區的數量為全市之最。 中壢區面積約為76.5km²，全區大致可以分成中壢、內壢、龍岡、大崙四個次分區，並以前三者為主要的市區和人口集中區域。";
-    }
-    else if (mo=="桃園區") {
-        content.innerHTML='<h2>桃園區是臺灣桃園市13區之一，為桃園市政府所在地。桃園區位於全市中央偏東北的位置，西鄰中壢區，北鄰蘆竹區，東鄰龜山區，南鄰八德區，東南鄰新北市鶯歌區。</h2>';
-
-    } else if (mo=="龍潭區") {
-        console.log('113');
-    } else {
-        console.log('1552');
+function hamfun(e){
+    el=e.target.value;
+    if(el==''){
+        alert('此處不可為空。')
     }
 }
+
+function frifun(e){
+    el=e.target.value;
+    if(el==''){
+        alert('此處不可為空。')
+    }
+}
+
+
+var sum=document.querySelector('.momo');
+sum.onclick=function(){
+    var hamSUM=document.querySelector('#hamID').value;
+    var friSUM=document.querySelector('#friID').value;
+    var sum1=parseInt(hamSUM)+parseInt(friSUM);
+    document.querySelector('.nono').textContent=sum1;
+}
+
