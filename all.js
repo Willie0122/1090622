68a4b51f9100dfd157cfd4f9b97caae2be709406
willie <ss3751@gmail.com>
2020-06-27 17:41:34 +0800

    mouse - 當滑鼠滑入指定內容時觸發

--- all.js
+++ all.js
@@ -1,28 +1,9 @@
-var ham=document.getElementById('hamID');
-var fri=document.getElementById('friID');
-ham.addEventListener('blur',hamfun,false);
-fri.addEventListener('blur',frifun,false);
-
-function hamfun(e){
-    el=e.target.value;
-    if(el==''){
-        alert('此處不可為空。')
-    }
-}
-
-function frifun(e){
-    el=e.target.value;
-    if(el==''){
-        alert('此處不可為空。')
-    }
-}
-
-
-var sum=document.querySelector('.momo');
-sum.onclick=function(){
-    var hamSUM=document.querySelector('#hamID').value;
-    var friSUM=document.querySelector('#friID').value;
-    var sum1=parseInt(hamSUM)+parseInt(friSUM);
-    document.querySelector('.nono').textContent=sum1;
-}
-
+var el=document.querySelector('.body');
+el.addEventListener('mousemove',function(e){
+    console.log("screenX="+e.screenX);
+    console.log("screenY="+e.screenY);
+    console.log("pageX="+e.pageX);
+    console.log("pageY="+e.pageY);
+    console.log("clientX="+e.clientX);
+    console.log("clientY="+e.clientY);  
+},false)
