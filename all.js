88dabc7677082ac1548f4da21db8449dcd8d3a37
willie <ss3751@gmail.com>
2020-07-17 11:50:59 +0800

    XMLhttprequest

--- all.js
+++ all.js
@@ -1,16 +1,3 @@
-var text = document.querySelector('.NameClass');
-var Btn = document.querySelector('.TextClass');
-var display=document.querySelector('.DisplayClass');
-
-
-
-function btn() {
-    var str=text.value;
-    localStorage.setItem('KEY-1',str);
-}
-
-Btn.addEventListener('click',btn,false);
-display.addEventListener('click',function(){
-    el=localStorage.getItem('KEY-1');
-    alert('你剛輸入的是'+el+'。')
-},false);
+var xhr = new XMLHttpRequest();
+xhr.open('get','https://hexschool.github.io/ajaxHomework/data.json','ture');
+xhr.send(null);
