5b5c53370cf8b335cca2e341f1f2ab5d78c8d990
willie <ss3751@gmail.com>
2020-07-19 21:22:53 +0800

    AJAX POST 寫法 (提供 API 練習)

--- all.js
+++ all.js
@@ -1,3 +1,16 @@
-var xhr = new XMLHttpRequest();
-xhr.open('get','https://hexschool.github.io/ajaxHomework/data.json','ture');
-xhr.send(null);
+var xhr=new XMLHttpRequest();
+xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+xhr.send('email=ss3751@gmail.com&password="1234');
+
+
+xhr.onload=function(){
+    console.log('111');
+    var str=JSON.parse(xhr.responseText);
+    console.log(str.message)
+     if(str.message=='此帳號已被使用'){
+       var momo=document.querySelector('.result');
+       momo.innerHTML="此帳號已被使用";
+    }
+
+}
