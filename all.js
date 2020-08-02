8dfcd54f827de46e683ea622ae2707033844f7a1
willie <ss3751@gmail.com>
2020-08-02 18:32:57 +0800

    2048遊戲加上戰艦遊戲

--- all.js
+++ all.js
@@ -1,16 +1,30 @@
-var xhr=new XMLHttpRequest();
-xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
-xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-xhr.send('email=ss3751@gmail.com&password="1234');
+var location1=3;
+var location2=4;
+var location3=5;
+var guess;
+var hits=0;
+var guesses=0;
+var isSunk=false;
 
-
-xhr.onload=function(){
-    console.log('111');
-    var str=JSON.parse(xhr.responseText);
-    console.log(str.message)
-     if(str.message=='此帳號已被使用'){
-       var momo=document.querySelector('.result');
-       momo.innerHTML="此帳號已被使用";
+while(isSunk==false){
+  guess=prompt("準備，攻擊!!(請輸入0~6)");
+  if(guess<0 || guess>6){
+    alert('請輸入0~6數字');
+  }else{
+    guesses=guesses+1;
+  
+  if(guess==location1 ||guess==location==2 ||guess==location3){
+    alert('擊中目標!!');
+    hits=hits+1;
+    if(hits==3){
+      isSunk=true;
+      alert('你擊敗全部戰艦');
     }
-
+  }else{
+    alert('未打中!!');
+  }
+  }
 }
+
+var stats="你猜了"+guesses+"次擊敗戰艦!"+"意味著你的猜測精準度為"+(3/guesses);
+alert(stats);
