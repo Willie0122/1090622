20f22ed4e7d0d19827d4504eb18c2708e03e4c15
willie <ss3751@gmail.com>
2020-08-02 19:39:20 +0800

    今彩539

--- all.js
+++ all.js
@@ -1,29 +1,11 @@
-var location1=Math.random();
-var location=Math.floor(location1);
-var guess;
-var hits=0;
-var guesses=0;
-var isSunk=false;
 
-while(isSunk==false){
-  guess=prompt("準備，攻擊!!(請輸入0~6)");
-  if(guess<0 || guess>6){
-    alert('請輸入0~6數字');
-  }else{
-    guesses=guesses+1;
-  
-  if(guess==location){
-    alert('擊中目標!!');
-    hits=hits+1;
-    if(hits==3){
-      isSunk=true;
-      alert('你擊敗全部戰艦');
-    }
-  }else{
-    alert('未打中!!');
-  }
-  }
+var num1=Math.floor(Math.random()*39);
+var num2=Math.floor(Math.random()*39);
+
+
+function myFunction() {
+  document.getElementById("demo").innerHTML="「"+num1+"」"+"、"+"「"+num2+"」"+"各兩元，全碰!!"
 }
 
-var stats="你猜了"+guesses+"次擊敗戰艦!"+"意味著你的猜測精準度為"+(3/guesses);
-alert(stats);
+
+
